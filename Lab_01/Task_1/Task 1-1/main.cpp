77bb92c4f10f6242d63b94aa2fef4e23518b189f
MarinaPaley <marina_paley@mail.ru>
2021-03-16 14:46:48 +0300

    Решена задача #1
    1. Выполнена проверка решения также и в Excel

--- Lab_01/Task_1/Task 1-1/main.cpp
+++ Lab_01/Task_1/Task 1-1/main.cpp
@@ -3,10 +3,19 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
 
 int main()
 {
-    const double x = 1.1;
-    std::cout << "Hello word!";
+    const auto x = 0.29;
+    const auto y = 3.7;
+    const auto z = -1.0;
+
+    const auto a = 3.0 * pow(x, y) * log(y) + exp(z * x);
+    const auto b = abs(2.0 * y * z) / (sin(x) * sin(x)) - x * x / 3.0;
+
+    cout << "x = " << x << ", y =  "<< y << ", z = " << z << "\n";
+    cout << "a = " << a << ", b = " << b << endl;
+
     return 0;
 }
